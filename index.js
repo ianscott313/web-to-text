1e3bdad045fd4b96bd0d54cae1c42e4b3dc77a74
ian scott <ianscott313@gmail.com>
2023-10-17 02:45:22 -0700

    Added comments

--- index.js
+++ index.js
@@ -2,33 +2,43 @@ const createBrowserless = require('browserless')
 const getHTML = require('html-get')
 const { convert } = require('html-to-text');
 
-// Spawn Chromium process once
+// Create a browserless instance
 const browserlessFactory = createBrowserless()
 
+/**
+ * Fetches HTML content from the specified URL
+ * @param {string} url - The URL to fetch content from
+ * @returns {Promise<string>} - The HTML content
+ */
 const getContent = async url => {
-  // create a browser context inside Chromium process
+  // Create a browser context inside the Chromium process
   const browserContext = browserlessFactory.createContext()
   const getBrowserless = () => browserContext
   const result = await getHTML(url, { getBrowserless })
-  // close the browser context after it's used
+  // Close the browser context after it's used
   await getBrowserless((browser) => browser.destroyContext())
   return result
 }
 
+/**
+ * Converts HTML content to plain text
+ * @param {string} url - The URL to fetch content from
+ * @returns {Promise<string>} - The converted plain text
+ */
 module.exports = async url => {
   getContent(url)
-  .then(content => {
-    const options = {
-      wordwrap: 130,
-    };
-    const text = convert(content.html, options);
-    // kill Chromium process 
-    browserlessFactory.close()
-    return text;
-  })
-  .catch(error => {
-    console.error(error)
-    // kill Chromium process 
-    browserlessFactory.close()
-  })
+    .then(content => {
+      const options = {
+        wordwrap: 130,
+      };
+      const text = convert(content.html, options);
+      // Kill the Chromium process 
+      browserlessFactory.close()
+      return text;
+    })
+    .catch(error => {
+      console.error(error)
+      // Kill the Chromium process 
+      browserlessFactory.close()
+    })
 }
